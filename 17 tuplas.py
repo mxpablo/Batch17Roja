23b8b9127886b585ff178b1f874fb2171cb370e8
JuanPablo Saldana <JP@JuanPablos-MacBook.local>
2017-11-06 21:36:20 -0600

    Cambie el nombre

---
+++ 17 tuplas.py
@@ -0,0 +1,10 @@
+tupla = (6,'Noviembre',2017) 
+'''Reconoce tupla porque son entre ()'''
+
+print(tupla)
+print(tupla[0])
+
+for i in tupla:
+	print(i)
+
+print(tupla[:2])
--- tuplas.py
+++
@@ -1,6 +0,0 @@
-tupla = (6,'Noviembre',2017)
-print(tupla)
-print(tupla[0])
-
-for i in tupla:
-	print(i)
